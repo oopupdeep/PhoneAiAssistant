cb403937ac8e4db5524ffcbeab1d125bb53067be
AsianPower <35124575+oopupdeep@users.noreply.github.com>
2025-07-05 01:44:15 -0700

    Fix SDK setup path

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+export ANDROID_SDK_ROOT="$HOME/android-sdk"
+PROJECT_DIR="$(pwd)"
+
+mkdir -p "$ANDROID_SDK_ROOT/cmdline-tools"
+cd "$ANDROID_SDK_ROOT/cmdline-tools"
+
+# remove any remnants of a previous installation
+rm -rf cmdline-tools latest
+
+curl -sSL -o tools.zip \
+  https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
+unzip -q tools.zip && rm tools.zip
+mkdir -p latest
+mv cmdline-tools/* latest/
+rmdir cmdline-tools
+
+export PATH="$ANDROID_SDK_ROOT/cmdline-tools/latest/bin:$PATH"
+yes | sdkmanager --licenses >/dev/null
+sdkmanager --sdk_root="$ANDROID_SDK_ROOT" \
+  "platform-tools" "platforms;android-35" "build-tools;35.0.0"
+
+echo "sdk.dir=$ANDROID_SDK_ROOT" > "$PROJECT_DIR/local.properties"
